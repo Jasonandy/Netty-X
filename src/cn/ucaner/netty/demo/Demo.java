4a169c8d4b72666f6dd125a162be2308a75caa06
Gooogle <jasonandy@hotmail.com>
2018-04-15 11:12:52 +0800

    :fire: init netty project by Jason

---
+++ src/cn/ucaner/netty/demo/Demo.java
@@ -0,0 +1,24 @@
+/**
+ * <html>
+ * <body>
+ *  <P> Copyright 1994-2018 JasonInternational </p>
+ *  <p> All rights reserved.</p>
+ *  <p> Created by Jason</p>
+ *  </body>
+ * </html>
+ */
+package cn.ucaner.netty.demo;
+
+/**     
+ * @Package：cn.ucaner.netty.demo   
+ * @ClassName：Demo   
+ * @Description：   <p> Netty Study </p>
+ * @Author： - Jason   
+ * @CreatTime：2018年4月15日  
+ * @Modify By：   
+ * @Modify marker：   
+ * @version    V1.0
+ */
+public class Demo {
+
+}
